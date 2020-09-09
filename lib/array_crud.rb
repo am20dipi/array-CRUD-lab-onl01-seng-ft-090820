06e98b42f500b6b12950d15b55e3c5ac14639e6a
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-09 19:42:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,6 +28,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index = ["Wow", "I", "am"]
   retrieve_element_from_index(array, 2)
 end
 
